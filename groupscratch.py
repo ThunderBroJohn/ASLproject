cc3f04de7c5b2f08932e6068f725c3a50fd625a8
BrettonSteiner <imbrettonsteiner@gmail.com>
2020-02-21 11:15:41 -0700

    Test push

--- groupscratch.py
+++ groupscratch.py
@@ -2,12 +2,12 @@
 #Kaylee Hertzog, John Miller, James Call, Bretton Steiner
 
 #imports
-import pyttsx3 
+import pyttsx3
 import numpy as np
 import cv2 #openCV
 
 #initialize Text to speach
-engine = pyttsx3.init() 
+engine = pyttsx3.init()
 engine.setProperty('rate', 165)#normal human speach is about 150 wpm
 
 def tts_test():
@@ -21,11 +21,13 @@ def run_camera_test():
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-        # Wait for 'q' to quit the program
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+
+        if (ret):
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            # Wait for 'q' to quit the program
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
@@ -38,4 +40,3 @@ def main():
 if __name__ == "__main__":
     # execute only if run as a script
     main()
-
