f150349533cb1061872218db1e3f5b13cff28629
John Miller <mil17003@byui.edu>
2020-02-21 10:32:06 -0700

    Create groupscratch.py
    Group scratch for testing perposes

---
+++ groupscratch.py
@@ -0,0 +1,41 @@
+#ASL Translation project
+#Kaylee Hertzog, John Miller, James Call, Bretton Steiner
+
+#imports
+import pyttsx3 
+import numpy as np
+import cv2 #openCV
+
+#initialize Text to speach
+engine = pyttsx3.init() 
+engine.setProperty('rate', 165)#normal human speach is about 150 wpm
+
+def tts_test():
+#text to speach usage
+    engine.say("Hello, how are you today?")
+    engine.runAndWait()
+
+def run_camera_test():
+    """ Live capture your laptop camera """
+    cap = cv2.VideoCapture(0)  # Notice the '0' instead of a filename
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        # Wait for 'q' to quit the program
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    tts_test()
+    run_camera_test()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
