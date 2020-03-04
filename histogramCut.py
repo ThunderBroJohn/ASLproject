41043a33a7e80b7f9f17e5455ad7f307087d05e6
kairuflayark <cal12011@byui.edu>
2020-03-03 18:41:20 -0700

    Create histogramCut.py

---
+++ histogramCut.py
@@ -0,0 +1,126 @@
+import cv2
+import numpy as np
+
+hand_hist = None
+traverse_point = []
+total_rectangle = 9
+hand_rect_one_x = None
+hand_rect_one_y = None
+
+hand_rect_two_x = None
+hand_rect_two_y = None
+
+
+def rescale_frame(frame, wpercent=130, hpercent=130):
+    width = int(frame.shape[1] * wpercent / 100)
+    height = int(frame.shape[0] * hpercent / 100)
+    return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+def draw_rect(frame):
+    rows, cols, _ = frame.shape
+    global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
+
+    hand_rect_one_x = np.array(
+        [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
+         12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
+
+    hand_rect_one_y = np.array(
+        [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
+         10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
+
+    hand_rect_two_x = hand_rect_one_x + 10
+    hand_rect_two_y = hand_rect_one_y + 10
+
+    for i in range(total_rectangle):
+        cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
+                      (hand_rect_two_y[i], hand_rect_two_x[i]),
+                      (0, 255, 0), 1)
+
+    return frame
+
+
+def hand_histogram(frame):
+    global hand_rect_one_x, hand_rect_one_y
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
+
+    for i in range(total_rectangle):
+        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
+                                          hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
+
+    hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
+
+
+def hist_masking(frame, hist):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+    cv2.filter2D(dst, -1, disc, dst)
+
+    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+
+    # thresh = cv2.dilate(thresh, None, iterations=5)
+
+    thresh = cv2.merge((thresh, thresh, thresh))
+
+    return cv2.bitwise_and(frame, thresh)
+
+
+def manage_image_opr(frame, hand_hist):
+    hist_mask_image = hist_masking(frame, hand_hist)
+    return hist_mask_image
+
+    # contour_list = contours(hist_mask_image)
+    # max_cont = max_contour(contour_list)
+
+    # cnt_centroid = centroid(max_cont)
+    # cv2.circle(frame, cnt_centroid, 5, [255, 0, 255], -1)
+
+    # if max_cont is not None:
+    #     hull = cv2.convexHull(max_cont, returnPoints=False)
+    #     defects = cv2.convexityDefects(max_cont, hull)
+    #     far_point = farthest_point(defects, max_cont, cnt_centroid)
+    #     print("Centroid : " + str(cnt_centroid) + ", farthest Point : " + str(far_point))
+    #     cv2.circle(frame, far_point, 5, [0, 0, 255], -1)
+    #     if len(traverse_point) < 20:
+    #         traverse_point.append(far_point)
+    #     else:
+    #         traverse_point.pop(0)
+    #         traverse_point.append(far_point)
+
+    #     draw_circles(frame, traverse_point)
+
+
+def main():
+    global hand_hist
+    is_hand_hist_created = False
+    capture = cv2.VideoCapture(0)
+
+    while capture.isOpened():
+        pressed_key = cv2.waitKey(1)
+        _, frame = capture.read()
+
+        if pressed_key & 0xFF == ord('z'):
+            is_hand_hist_created = True
+            hand_hist = hand_histogram(frame)
+
+        if is_hand_hist_created:
+            frame = hist_masking(frame, hand_hist)
+
+        else:
+            frame = draw_rect(frame)
+
+        cv2.imshow("Live Feed", rescale_frame(frame))
+
+        if pressed_key == 27:
+            break
+
+    cv2.destroyAllWindows()
+    capture.release()
+
+
+if __name__ == '__main__':
+    main()
