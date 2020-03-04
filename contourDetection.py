935279fee8576d1cb0a461ece70d333755045c94
BrettonSteiner <imbrettonsteiner@gmail.com>
2020-03-04 10:11:39 -0700

    Combined two ideas into one.

--- contourDetection.py
+++ contourDetection.py
@@ -6,16 +6,91 @@ import pyttsx3
 import numpy as np
 import cv2 #openCV
 
+hand_hist = None
+traverse_point = []
+total_rectangle = 9
+hand_rect_one_x = None
+hand_rect_one_y = None
+
+hand_rect_two_x = None
+hand_rect_two_y = None
+
+def rescale_frame(frame, wpercent=130, hpercent=130):
+    width = int(frame.shape[1] * wpercent / 100)
+    height = int(frame.shape[0] * hpercent / 100)
+    return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+def draw_rect(frame):
+    rows, cols, _ = frame.shape
+    global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
+
+    hand_rect_one_x = np.array(
+        [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
+         12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
+
+    hand_rect_one_y = np.array(
+        [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
+         10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
+
+    hand_rect_two_x = hand_rect_one_x + 10
+    hand_rect_two_y = hand_rect_one_y + 10
+
+    for i in range(total_rectangle):
+        cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
+                      (hand_rect_two_y[i], hand_rect_two_x[i]),
+                      (0, 255, 0), 1)
+
+    return frame
+
+
+def hand_histogram(frame):
+    global hand_rect_one_x, hand_rect_one_y
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
+
+    for i in range(total_rectangle):
+        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
+                                          hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
+
+    hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
+
+
+def hist_masking(frame, hist):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+    cv2.filter2D(dst, -1, disc, dst)
+
+    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+
+    thresh = cv2.dilate(thresh, None, iterations=5)
+
+    thresh = cv2.merge((thresh, thresh, thresh))
+
+    return cv2.bitwise_and(frame, thresh)
+
+def manage_image_opr(frame, hand_hist):
+    hist_mask_image = hist_masking(frame, hand_hist)
+    return hist_mask_image
+
+def rectangle_masking(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 0, 254, cv2.THRESH_BINARY)
+    # Find the contours
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cont = contours[0]
+    x,y,w,h = cv2.boundingRect(cont)
+
+    # Consider trying a rectangle of a consistent size
+    
+    return frame[y:y+h,x:x+w]
+
 # Code taken from http://creat-tabu.blogspot.com/2013/08/opencv-python-hand-gesture-recognition.html
-
-def main():
-    # Creating camera object
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        # Read the frame
-        ret, img = cap.read()
-
-        # Apply filters to clean up image
+def contourDetection(img):
+    # Apply filters to clean up image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         ret, thresh1 = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
@@ -49,29 +124,54 @@ def main():
         cv2.drawContours(drawing, [cont], 0, (0, 255, 0), 2)
         cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 2)
 
-        # Find convexity defects
-        hull = cv2.convexHull(cont, returnPoints=False)
-        defects = cv2.convexityDefects(cont, hull)
+        # # Find convexity defects
+        # hull = cv2.convexHull(cont, returnPoints=False)
+        # defects = cv2.convexityDefects(cont, hull)
 
-        # Plot defects
-        min_d = 0
-        max_d = 0
-        i = 0
-        for i in range(defects.shape[0]):
-            s, e, f, d = defects[i,0]
-            start = tuple(cont[s][0])
-            end = tuple(cont[e][0])
-            far = tuple(cont[f][0])
-            dist = cv2.pointPolygonTest(cont, centr, True)
-            cv2.line(drawing, start, end, [0, 255, 0], 2)
-            cv2.circle(drawing, far, 5, [0, 0, 255], -1)
-        # print(i)
+        # # Plot defects
+        # min_d = 0
+        # max_d = 0
+        # i = 0
+        # for i in range(defects.shape[0]):
+        #     s, e, f, d = defects[i,0]
+        #     start = tuple(cont[s][0])
+        #     end = tuple(cont[e][0])
+        #     far = tuple(cont[f][0])
+        #     dist = cv2.pointPolygonTest(cont, centr, True)
+        #     cv2.line(drawing, start, end, [0, 255, 0], 2)
+        #     cv2.circle(drawing, far, 5, [0, 0, 255], -1)
+        # # print(i)
+        return drawing
 
-        # Display the frame
-        cv2.imshow('input', drawing)
-        k = cv2.waitKey(10)
-        if k == 27:
+def main():
+    global hand_hist
+    is_hand_hist_created = False
+    capture = cv2.VideoCapture(0)
+
+    while capture.isOpened():
+        pressed_key = cv2.waitKey(1)
+        _, frame = capture.read()
+
+        if pressed_key & 0xFF == ord('z'):
+            is_hand_hist_created = True
+            hand_hist = hand_histogram(frame)
+
+        if is_hand_hist_created:
+            masked = hist_masking(frame, hand_hist)
+            frame = rectangle_masking(masked)
+            # frame = contourDetection(frame)
+
+        else:
+            frame = draw_rect(frame)
+
+        cv2.imshow("Live Feed", rescale_frame(frame))
+
+        if pressed_key == 27:
             break
+
+    cv2.destroyAllWindows()
+    capture.release()
+        
 
 if __name__ == "__main__":
     # execute only if run as a script
