dc469f74976c9dbf49a3a2aad1058d0e883d646e
John Miller <mil17003@byui.edu>
2020-03-18 21:48:06 -0600

    init compare data
    init compare data

--- prototype1.py
+++ prototype1.py
@@ -15,42 +15,50 @@ import imageProcesses
 # engine = pyttsx3.init()
 # engine.setProperty('rate', 165)#normal human speach is about 150 wpm
 
-"""
+
 #Function IN PROGRESS
 def initialize_comparison_library():
     #abc... and bs(backspace) and space
     alphabetList = [(cv2.imread("ASLproject/ASLproject/edgePreprocess/a1.png"), "a")]
     alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"b"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/c1.png"),"c"]))
 
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"d"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"e"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"f"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"g"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"h"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"i"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"j"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"k"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"l"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"m"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"n"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"o"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"p"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"q"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"r"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"s"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"t"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"u"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"v"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"w"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"x"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"y"]))
-    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/b1.png"),"z"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/c1.png"),"c"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/c2.png"),"c"]))
+
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/d1.png"),"d"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/e1.png"),"e"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/f1.png"),"f"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/g1.png"),"g"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/h1.png"),"h"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/i1.png"),"i"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/j1.png"),"j"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/k1.png"),"k"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/l1.png"),"l"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/m1.png"),"m"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/n1.png"),"n"]))
+
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/o1.png"),"o"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/o2.png"),"o"]))
+
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/p1.png"),"p"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/q1.png"),"q"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/r1.png"),"r"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/s1.png"),"s"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/t1.png"),"t"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/u1.png"),"u"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/v1.png"),"v"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/w1.png"),"w"]))
+
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/x1.png"),"x"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/x1.png"),"x"]))
+
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/y1.png"),"y"]))
+    alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/z1.png"),"z"]))
 
     alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/_1.png"),"_"]))
     alphabetList.append(([cv2.imread("ASLproject/ASLproject/edgePreprocess/bs1.png"),"bs"]))
     return alphabetList
-"""
+
 
 #OUTPUT functions
 #This function will run the text to speach output
@@ -71,16 +79,23 @@ before looking for the next letter.
 
 returns letter (and lookForLetter to false if found? (Look reset))
 """
-def translateSymbol(frame, lookForLetter):
-    if(not lookForLetter):
-        return ""
-    #ADD TRANSLATION LOGIC HERE
     
 def image_compare(image1, image2):
     sum_diff = np.sum(cv2.absdiff(image1, image2))
     return sum_diff
 
+def translateSymbol(frame, lookForLetter, alphabetList):
+    if(not lookForLetter):
+        return ""
+    #ADD TRANSLATION LOGIC HERE
+        minMatch = num 
+        for letter in alphabetList:
+            pass #WRITE COMPARE
+            
+
+
 def main():
+    alphabetList = initialize_comparison_library()
 
     #string for use in output
     letterString = "test"
